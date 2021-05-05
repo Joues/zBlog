6ea3b7c0e38433cafdfded25be98e4f16547f8b2
joues <yihangjou@outlook.com>
2021-05-05 09:49:18 +0800

    表名修改SQL查询

--- zblog/src/main/java/cn/ityihang/zblog/blog/mapper/BlogMapper.java
+++ zblog/src/main/java/cn/ityihang/zblog/blog/mapper/BlogMapper.java
@@ -18,12 +18,12 @@ import java.util.Map;
  */
 public interface BlogMapper extends BaseMapper<BlogInfo> {
 
-    @Select("select id,title from blog s\n" +
+    @Select("select id,title from blog_info s\n" +
             "ORDER BY ABS(NOW() - s.create_time) ASC\n" +
             "limit #{sizeNumber};")
     List<Map<String, Object>> getBlogNews(@Param("sizeNumber") Integer sizeNumber);
 
-    @Select("SELECT A.id,A.title from blog A, blog_poll B " +
+    @Select("SELECT A.id,A.title from blog_info A, blog_poll B " +
             "where A.id = B.blog_id GROUP BY B.blog_id " +
             "ORDER BY COUNT(B.id) DESC " +
             "LIMIT #{sizeNumber}")
